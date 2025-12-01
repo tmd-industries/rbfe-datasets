2c2e71f95bca1e61e760eab63717b940889e6941
badisa <badisa@users.noreply.github.com>
2025-12-01 10:30:17 -0700

    Add Amber AM1BCC charges (#4)
    * Add Amber AM1BCC charges to datasets
    
    * Prefer AM1BCC charges over NAGL charges
    * Make ligands.sdf the standard name for ligands
    
    * Update CMET Neutral map to be k min cut = 3
    
    * Add tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,8 +1,11 @@
+import json
 import os
+from pathlib import Path
 
+import networkx as nx
 import pytest
 from tmd.constants import DEFAULT_PROTEIN_FF, DEFAULT_WATER_FF
-from tmd.fe.utils import read_sdf
+from tmd.fe.utils import get_mol_name, read_sdf, read_sdf_mols_by_name
 from tmd.md.builders import build_protein_system
 
 datasets_dir = os.path.dirname(os.path.dirname(__file__))
@@ -10,6 +13,8 @@ datasets_dir = os.path.dirname(os.path.dirname(__file__))
 KJ_EXPERIMENTAL_LABEL = "kJ/mol experimental dG"
 EXPERIMENTAL_FIELD = "experimental dG reference field"
 EXPERIMENTAL_UNIT_FIELD = "experimental dG reference units"
+
+AMBER_CHARGES_FIELD = "AmberAM1BCCCache"
 
 
 def files_with_ext(dirname, ext):
@@ -19,10 +24,32 @@ def files_with_ext(dirname, ext):
                 yield os.path.join(root, file)
 
 
+def build_graph_from_edges(edges: list[dict]) -> nx.Graph:
+    g = nx.Graph()
+    for edge_data in edges:
+        g.add_edge(edge_data["mol_a"], edge_data["mol_b"], core=edge_data.get("core", None))
+    return g
+
+
+@pytest.mark.parametrize("ds", list(files_with_ext(datasets_dir, "map.json")))
+def test_maps_are_connected(ds):
+    directory = Path(ds).parent
+    ligands_path = directory / "ligands.sdf"
+    assert (ligands_path).is_file()
+    with open(ds) as ifs:
+        edges = json.load(ifs)
+    assert len(edges) > 0
+    mols_by_name = read_sdf_mols_by_name(ligands_path)
+    graph = build_graph_from_edges(edges)
+    for node in graph.nodes:
+        assert node in mols_by_name
+    assert nx.edge_connectivity(graph) == 3
+
+
 @pytest.mark.parametrize("ds", list(files_with_ext(datasets_dir, ".sdf")))
 def test_datasets_have_experimental_labels(ds):
     # Don't remove hydrogens, throws warnings
-    mols = list(read_sdf(ds))
+    mols = read_sdf(ds)
     has_label = False
     for mol in mols:
         kj_label = mol.GetProp(KJ_EXPERIMENTAL_LABEL)
@@ -34,6 +61,13 @@ def test_datasets_have_experimental_labels(ds):
         except ValueError:
             pass
     assert has_label
+
+
+@pytest.mark.parametrize("ds", list(files_with_ext(datasets_dir, ".sdf")))
+def test_datasets_have_amber_charges(ds):
+    mols = read_sdf(ds)
+    for mol in mols:
+        assert mol.HasProp(AMBER_CHARGES_FIELD), f"Mol {get_mol_name(mol)} doesn't have amber charges"
 
 
 @pytest.mark.parametrize("pdb", list(files_with_ext(datasets_dir, ".pdb")))
